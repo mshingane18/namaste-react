766dd188bea035101180eb405600e42b5e93f5c9
mshingane18 <shinganemahesh18@gmail.com>
2025-06-10 10:40:27 +0530

    episod-01

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h1", {}, "i am h1 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h1", {}, "i am h1 tag"),
+  ]),
+]);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
