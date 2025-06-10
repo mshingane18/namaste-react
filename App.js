33d227ed9b99aa6c4be3e580cc084299c7cd5c81
mshingane18 <shinganemahesh18@gmail.com>
2025-06-10 15:05:59 +0530

    episod-02

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h1", {}, "Hi mahesh, Pune"),
     React.createElement("h1", {}, "i am h1 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
