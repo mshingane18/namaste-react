db76ab14bee79f7096d3b04c0d71bc5a27a92a91
Cybage_Mahesh <mahesh.shingane@spaceiq.com>
2025-06-12 11:35:40 +0530

    episod-03-2

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const head = (<h1>Hello</h1>)
 
+const Title = () =>  (
+    <h1 className="title" id="title">ReactJS</h1>
+  );
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is my first component in ReactJS"
-);
-
-const jsxHeading = <h1 id="heading" className="heading">Namste React using JSX</h1>
-console.log(jsxHeading);
+const HeadingComponent = ()=> (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title> </Title>
+      <h1 id="heading">This is my first component in ReactJS</h1>
+  </div>
+  );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
